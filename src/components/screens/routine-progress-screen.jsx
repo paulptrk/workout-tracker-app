3965327478d5137aa30d8101f84429bfbfa3b85f
Paul Patrick <paul7098@hotmail.com>
2025-11-17 17:48:14 -0500

    Add exercise details

--- src/components/screens/routine-progress-screen.jsx
+++ src/components/screens/routine-progress-screen.jsx
@@ -1,18 +1,35 @@
 
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Header from "@/components/ui/Header";
+import { Separator } from "@/components/ui/separator";
 import { mockRoutines } from "@/data/mockRoutines";
 
 export default function RoutineProgressScreen({ routineId, onBack }) {
     const routine = mockRoutines.find((routine) => routine.id === routineId);
     return (
-        <div>
-            {routine.exercises.map(exercise => (
-                <Card>
-                    <CardHeader>
-                        <CardTitle>{exercise.name}</CardTitle>
-                    </CardHeader>
-                </Card>
-            ))}
-        </div>
+        <>
+            <Header
+                title={routine.title}
+                description='In progress'
+                hasBackButton={true}
+                onBackButtonClick={onBack}
+            />
+            <Separator className="mb-6" />
+            <div className="md:max-w-xl lg:max-w-3xl">
+                {routine.exercises.map((exercise, i) => (
+                    <Card className='m-6 flex gap-4 w-full flex flex-col'>
+                        <CardHeader>
+                            <CardTitle>{exercise.name}</CardTitle>
+                            <CardDescription>
+                                {exercise.sets} sets x {exercise.reps} reps
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            Set {i + 1}
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+        </>
     )
 }
